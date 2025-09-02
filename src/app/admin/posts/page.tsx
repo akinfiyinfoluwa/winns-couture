cc476306237a8a2bbac2485206d8737d16ae5dc3
akinfiyinfoluwa <apostleofwords@gmail.com>
2025-09-03 00:44:52 +0100

    corrected the posts page

---
+++ src/app/admin/posts/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {}
+
+function Page(props: Props) {
+    const {} = props
+
+    return (
+        <div>
+            <h1>Posts</h1>
+        </div>
+    )
+}
+
+export default Page
